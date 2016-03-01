e850c423f61976b3d13c479e56b4d9ad9d9d2667
andrew-travis-wadman <wadman@utexas.edu>
2016-03-01 15:38:38 -0600

    Update calculator

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,4 +1,48 @@
 //calculator
 
-var store = "";
+var equation = "";
+var arr = ["0","1","2","3","4","5","6","7","8","9",".","+","-","*","%"];
+var value = "";
+var answer = "";
+
+$(document).ready(function(){
+	$("button").click(function(){
+		
+		value = this.innerHTML;
+		
+
+		if ($.inArray(value,arr) !== -1){
+			equation += value;
+			document.getElementById("answer").innerHTML = equation;
+		}
+		if (value === "C") {
+			equation = "";
+			document.getElementById("answer").innerHTML = equation;
+		}
+		if (value === "="){
+			answer = eval(equation);
+			document.getElementById("answer").innerHTML = answer;
+			equation = "";
+		}
+
+
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
 
